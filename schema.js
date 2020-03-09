bf94ead54c0382a3e074863a0118ce380721e24d
Daniel Hsieh <daniel.x.hsieh@gmail.com>
2020-03-09 11:38:34 -0400

    GraphQL server is now set up - hello world

---
+++ schema.js
@@ -0,0 +1,57 @@
+const axios = require('axios');
+const {
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLList ,
+  GraphQLSchema
+} = require('graphql');
+
+//Launch Type
+const LaunchType = new GraphQLObjectType({
+  name: 'Launch',
+  fields: () => ({
+    //each field we have will be a type
+    //refer to http://github.com/r-spacex/SpaceX-API
+
+    //https://api.spacexdata.com/v3/launches
+    //is going to be our endpoint
+
+    flight_number: {type: GraphQLInt},
+    mission_name: {type: GraphQLString},
+    launch_year: {type: GraphQLString},
+    launch_date_local: {type: GraphQLString},
+    launch_success: {type: GraphQLBoolean},
+    rocket: {type: RocketType}
+  })
+});
+
+const RocketType = new GraphQLObjectType({
+  name: 'Rocket',
+  fields: () => ({
+    rocket_id: {type: GraphQLString},
+    rocket_name: {type: GraphQLString},
+    rocket_type: {type: GraphQLString}
+  })
+});
+
+//Root Query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    launches: {
+      type: new GraphQLList(LaunchType),
+        resolve(parent, args){
+          return axios.get('https://api.spacexdata.com/v3/launches')
+            .then(res=>res.data);
+
+        }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
+
